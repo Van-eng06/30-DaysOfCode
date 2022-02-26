21ef802cf9e48a7108f58cc3ba03f4414f02e2c3
Vandana Chauhan <19bcs4532@cuchd.in>
2022-02-26 18:57:57 +0530

    Create D10 - 1523. Count Odd Numbers in an Interval Range.cpp

---
+++ CODE/D10 - 1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        long int c=(high-low)/2;;
+        if(low%2==0 && high%2==0)
+        { 
+            return c;
+        }
+        else
+            return c+1;
+            
+            
+        return c;
+    }
+};
